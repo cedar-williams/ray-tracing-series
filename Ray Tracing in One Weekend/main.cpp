e1cd3fcaa6f0abf98fb566ce49f24638bac6cddb
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-10-30 17:09:16 -0400

    Initial Commit

---
+++ Ray Tracing in One Weekend/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ray Tracing in One Weekend
+//
+//  Created by Cedar Williams on 10/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
