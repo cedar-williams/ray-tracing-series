c82a60b95e492159efd7f9e00c98116835519083
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-10-30 20:01:50 -0400

    Update color.h

---
+++ Ray Tracing in One Weekend/color.h
@@ -0,0 +1,31 @@
+//
+//  color.h
+//  Ray Tracing in One Weekend
+//
+//  Created by Cedar Williams on 10/30/25.
+//
+
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+    
+    // Translate [0,1] component values to the byte range [0,255]
+    int rbyte = int(255.99 * r);
+    int gbyte = int(255.99 * g);
+    int bbyte = int(255.99 * b);
+    
+    // Write ou the pixel color components
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
