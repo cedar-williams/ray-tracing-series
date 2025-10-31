d3e14d3cf6b594d1ce346975594991fd8d1407d1
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-10-31 10:43:35 -0400

    Add antialiasing

--- Ray Tracing in One Weekend/color.h
+++ Ray Tracing in One Weekend/color.h
@@ -8,6 +8,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
@@ -18,9 +19,10 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto b = pixel_color.z();
     
     // Translate [0,1] cstatic_cast<int>omponent values to the byte range [0,255]
-    int rbyte = static_cast<int>(255.99 * r);
-    int gbyte = static_cast<int>(255.99 * g);
-    int bbyte = static_cast<int>(255.99 * b);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = static_cast<int>(256 * intensity.clamp(r));
+    int gbyte = static_cast<int>(256 * intensity.clamp(g));
+    int bbyte = static_cast<int>(256 * intensity.clamp(b));
     
     // Write ou the pixel color components
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
--- Ray Tracing in One Weekend/main.cpp
+++ Ray Tracing in One Weekend/main.cpp
@@ -33,8 +33,9 @@ int main(int argc, const char * argv[]) {
     
     camera cam;
     
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
     
     cam.render(world);
     
