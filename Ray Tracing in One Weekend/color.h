61d5cd9a501e3eeafb0963d28a66b0eae05860c6
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-10-30 20:46:51 -0400

    Update typecasting

--- Ray Tracing in One Weekend/color.h
+++ Ray Tracing in One Weekend/color.h
@@ -19,10 +19,10 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
     
-    // Translate [0,1] component values to the byte range [0,255]
-    int rbyte = int(255.99 * r);
-    int gbyte = int(255.99 * g);
-    int bbyte = int(255.99 * b);
+    // Translate [0,1] cstatic_cast<int>omponent values to the byte range [0,255]
+    int rbyte = static_cast<int>(255.99 * r);
+    int gbyte = static_cast<int>(255.99 * g);
+    int bbyte = static_cast<int>(255.99 * b);
     
     // Write ou the pixel color components
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
