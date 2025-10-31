44b96be14180fad659fbd7175891a8452616c266
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-10-31 01:03:34 -0400

    Create and use custom std lib

---
+++ Ray Tracing in One Weekend/sphere.h
@@ -0,0 +1,53 @@
+//
+//  sphere.h
+//  Ray Tracing in One Weekend
+//
+//  Created by Cedar Williams on 10/30/25.
+//
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+
+
+class sphere: public hittable {
+public:
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = h * h - a * c;
+        if (discriminant < 0) {
+            return false;
+        }
+        
+        auto sqrtd = std::sqrt(discriminant);
+        
+        // Find the neared root in the acceptable range
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root) {
+                return false;
+            }
+        }
+        
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        
+        return true;
+    }
+    
+private:
+    point3 center;
+    double radius;
+    
+};
+
+#endif // !SPHERE_H
