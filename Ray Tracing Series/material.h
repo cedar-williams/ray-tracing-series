f60348107cae8a6e4deb39969c38ba8786f4fac7
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-11-12 09:25:47 -0500

    Add motion blur

--- Ray Tracing Series/material.h
+++ Ray Tracing Series/material.h
@@ -36,7 +36,7 @@ public:
         if (scatter_direction.near_zero())
             scatter_direction = rec.normal;
         
-        scattered = ray(rec.p, scatter_direction);
+        scattered = ray(rec.p, scatter_direction, r_in.time());
         attenuation = albedo;
         return true;
     }
@@ -52,7 +52,7 @@ public:
     const override {
         vec3 reflected = reflect(r_in.direction(), rec.normal);
         reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected, r_in.time());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
@@ -84,7 +84,7 @@ public:
             direction = refract(unit_direction, rec.normal, ri);
         }
         
-        scattered = ray(rec.p, direction);
+        scattered = ray(rec.p, direction, r_in.time());
         return true;
     }
     
